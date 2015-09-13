066247975d4ee80298dedf3a19661e5632e26610
shaheenashaik <shaheenashaik03@gmail.com>
2015-09-13 15:04:47 +0530

    Changes for the TestAction class

--- first-project/src/com/first/action/TestAction.java
+++ first-project/src/com/first/action/TestAction.java
@@ -17,11 +17,19 @@ public class TestAction {
 	{
 		System.out.println("hai");
 		return 5;
+		
+	}
+	public String sha(String h)
+	{
+		System.out.println("this is mew program");
+		return h;
 	}
 	
 	public static void main(String[] args) {
 		TestAction t=new TestAction();
 		System.out.println(t.test(5));
+		System.out.println(t.sha("im"));
+		
 		
 	}
 
